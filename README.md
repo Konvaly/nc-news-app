6cecf01f5bbdba7d7424dd65b43b77f42c81e6b1
Konvaly <konvaly@gmail.com>
2025-02-28 16:57:33 +0000

    Completed tasks 1-2, created 4 tables

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # NC News Seeding
+1. For connection locally to databases with choosing to test or development environment you need to create in the main root two files: 
+       .env.test
+       .env.development
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+2. Write down into these files 
+
+     PGDATABASE=
+
+and assign to PGDATABASE the database's name which you can find in the file db/setup-dbs.sql accordingly to the .env file's name (considering about what kind of environment you need to mention in the .env file: test or development).
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,45 @@
 const db = require("../connection")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db.query("DROP TABLE IF EXISTS comments")
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS articles")
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS users")
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS topics")
+    })
+    .then(() => {
+      return db.query(
+        "CREATE TABLE topics(slug VARCHAR(100) PRIMARY KEY, description VARCHAR NOT NULL, img_url VARCHAR(1000))"
+      )
+    })
+    .then(() => {
+      return db.query(
+        "CREATE TABLE users(username VARCHAR(50) PRIMARY KEY, name VARCHAR(100) NOT NULL, avatar_url VARCHAR(1000))"
+      )
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles(
+        article_id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, 
+        topic VARCHAR (100) REFERENCES topics(slug) ON DELETE CASCADE,
+        author VARCHAR(50) REFERENCES users(username) ON DELETE CASCADE,
+        body TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        votes INT DEFAULT 0,
+        article_img_url VARCHAR(1000))`)
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments(
+         comment_id SERIAL PRIMARY KEY,
+         article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
+         body TEXT NOT NULL,
+         votes INT DEFAULT 0,
+         author VARCHAR(50) REFERENCES users(username) ON DELETE CASCADE,
+         created_at TIMESTAMP DEFAULT NOW()
+        )`)
+    })
 };
 module.exports = seed;
