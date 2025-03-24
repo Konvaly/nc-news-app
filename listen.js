5ca374ca2a8efe3cbea7f075ee4aa807749a1544
Konvaly <konvaly@gmail.com>
2025-03-24 15:08:51 +0000

    Added setup file for hosting

--- listen.js
+++ listen.js
@@ -1,11 +1,14 @@
 const app = require("./app");
 
-app.listen(8080, (err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("app listening on 8080")
-    }
-})
+// app.listen(8080, (err) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("app listening on 8080")
+//     }
+// })
+const { PORT = 8080 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 
 module.exports = app;
